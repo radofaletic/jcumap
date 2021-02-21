de591113b5b51f0b22e8b901acd3461132401951
Rado Faletič <radofaletic@gmail.com>
2021-02-21 04:43:20 +0000

    config.php created online with Bitbucket

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+error_reporting(E_ALL);
+
+
+
+
+
+// the relative URL
+//  typically will be '/', for deployment on a stand-alone site
+$urlPrefix = '/professional_skills_mapping/';
+
+// the type of URLs to use
+//  options are 'pretty' (e.g. /get/variables/go/into/url/string) and 'php' (e.g. /?x1=get&x2=variables&x3=go&x4=into&x5=url&x6=string)
+$urlDisplayType = 'pretty';
